f361493002530d2fefcd4a77aa974a3f285fabba
Renao Chetri <63331185+ChhetriPrem@users.noreply.github.com>
2025-05-14 19:37:51 +0530

    Update Auth.jsx

--- src/components/AuthPage/Auth.jsx
+++ src/components/AuthPage/Auth.jsx
@@ -442,7 +442,7 @@ setSignature(sigBase64)
         </h2>
         <div className="flex flex-col md:flex-row gap-8 items-center justify-center">
           <img
-            src={/image.png} // Replace with your screenshot
+            src="/image.png"
             alt="SkillMint Wallet Linking"
             className="rounded-2xl shadow-2xl w-full md:w-1/2"
           />
