8a4e8ff4fee2a5b7d1ee3430247f894a54d4a1ae
Renao Chetri <63331185+ChhetriPrem@users.noreply.github.com>
2025-05-14 15:06:00 +0530

    Update githubOAuth.js

--- src/utils/githubOAuth.js
+++ src/utils/githubOAuth.js
@@ -1,7 +1,6 @@
 // utils/githubOAuth.js
 const CLIENT_ID = "Ov23liifKtyIL6YtXpDr";
-const REDIRECT_URI = "https://localhost:5173/oauth-callback"; // Change for prod
-
+const REDIRECT_URI = "https://skillmint-fe.vercel.app/oauth-callback"; 
 export function getGithubAuthUrl() {
   const scope = "read:user";
   return `https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&scope=${scope}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}`;
